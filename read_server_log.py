9564b3115e475df10fba648bb2886181b5679bf8
sacross93 <wlsdud1517@naver.com>
2023-11-15 08:50:12 +0900

    Remove unnecessary code

--- read_server_log.py
+++ read_server_log.py
@@ -81,27 +81,19 @@ all_df[all_df['REQUEST_PATH'] == '/client/report_status']['VALUE_PARAM'].unique(
 
 data_list = []
 
-# Regular expression pattern to match the problematic JSON-like strings
 pattern = re.compile(r'\"data\":\"(.*?)\",')
 
-# Loop through each line in the log
 for line in log_lines:
-    # Split the line into timestamp, hostname, and JSON data
     timestamp_str, _, json_data = line.strip().split('\t', 2)
-
-    # Convert single quotes to double quotes in JSON data
     json_data = json_data.replace("'", '"')
 
-    # Extract the value of the 'data' field as a separate string
     match = pattern.search(json_data)
     if match:
         data_str = match.group(1)
 
-        # Replace the original 'data' string with the properly escaped string
         data_str = data_str.replace('"', '\\"')
         json_data = json_data.replace(f'"{data_str}"', f'"{data_str}"')
 
-    # Parse the JSON data
     try:
         data_dict = json.loads(json_data)
     except json.JSONDecodeError as e:
@@ -120,26 +112,20 @@ for line in log_lines:
             continue
             # break
 
-    # Extract relevant information from the parsed data
     request_path = data_dict.get("REQUEST_PATH", "")
 
-    # Add key-value pairs from the current log entry to the list
     for key, value in data_dict.items():
-        # Skip known keys like "REQUEST_PATH" and "RESPONSE_STATUS"
         if key not in ("REQUEST_PATH", "RESPONSE_STATUS"):
             data_type = type(value).__name__
 
-            # If the data type is a dictionary, flatten it and store each key-value pair separately
             if data_type == "dict":
                 for sub_key, sub_value in value.items():
                     data_list.append(
                         {"REQUEST_PATH": request_path, "KEY": key, "VALUE_PARAM": sub_key, "VALUE": sub_value})
             else:
-                # If the data type is not a dictionary, store the string representation of the value
                 data_list.append(
                     {"REQUEST_PATH": request_path, "KEY": key, "DATA_TYPE": data_type, "VALUE": str(value)})
 
-# Create a DataFrame from the collected data types and values
 df = pd.DataFrame(data_list)
 
 df['REQUEST_PATH'].unique()
